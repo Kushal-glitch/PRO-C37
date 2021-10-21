7d24b7caed0b9d9c82bf53c10a0823dca6dadfbb
Kushal-glitch <78793455+Kushal-glitch@users.noreply.github.com>
2021-10-21 16:22:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ var question, contestant, quiz;
 
 function setup(){
   canvas = createCanvas(850,400);
-  database = firebase.database();
+database = firebase.database();
   quiz = new Quiz();
   quiz.getState();
   quiz.start();
